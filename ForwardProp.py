cfd6a1346b635b3b0aa2ba9673c8316c9844e8eb
willysantoso05 <willysantoso05@gmail.com>
2020-09-19 23:20:53 +0700

    update cnn

--- ForwardProp.py
+++ ForwardProp.py
@@ -4,26 +4,6 @@ import sys
 import math
 from PIL import Image
 
-# def load_and_pad_input(image_path, padding=2, padded_number=0):
-#     raw_img = Image.open(image_path, 'r')
-#     mat_img = np.transpose(list(raw_img.getdata())) \
-#                 .reshape(3, raw_img.size[0], raw_img.size[1])
-
-#     return pad_matrix(mat_img, padding, padded_number)
-
-# def pad_matrix(mat_img, padding, padded_number):
-#     def pad_with(vector, pad_width, iaxis, kwargs):
-#         pad_value = kwargs.get('padder', padded_number)
-#         vector[:pad_width[0]] = pad_value
-#         vector[-pad_width[1]:] = pad_value
-
-#     return np.pad(
-#         mat_img, 
-#         ((0, 0), (padding, padding), (padding, padding)), 
-#         pad_with, 
-#         padder=padded_number
-#     )
-
 def load_image(image_path):
     raw_img = skimage.io.imread(image_path)
     return raw_img
@@ -47,79 +27,68 @@ def padding(input_matrix, pad_layer=2, padded_number=0):
 
     return padded_result
 
-def conv_(input_matrix, conv_filter, stride):
-    filter_size = conv_filter.shape[1]
-    result = np.zeros((input_matrix.shape))
-    #Looping through the image to apply the convolution operation.
-    # iterate row
-    for r in np.uint16(np.arange(
-        filter_size/2.0, 
-        input_matrix.shape[0]-filter_size/2.0+1,
-        stride
-        )):
-        # iterate column
-        for c in np.uint16(np.arange(
-            filter_size/2.0, 
-            input_matrix.shape[1]-filter_size/2.0+1,
-            stride
-            )):
-            curr_region = input_matrix[r-np.uint16(np.floor(filter_size/2.0)):r+np.uint16(np.ceil(filter_size/2.0)), 
-                            c-np.uint16(np.floor(filter_size/2.0)):c+np.uint16(np.ceil(filter_size/2.0))]
+def conv_calculation(input_matrix, conv_filter, stride=1):
+    #input_matrix shape = (mxn)
+    output_shape = (((input_matrix.shape[0] - conv_filter.shape[0]) // stride + 1),
+                    ((input_matrix.shape[1] - conv_filter.shape[1]) // stride + 1))
 
+    output_matrix = np.zeros(output_shape)
+
+    input_length_idx = 0; output_length_idx = 0
+    while(input_length_idx < input_matrix.shape[0] - conv_filter.shape[0] + 1):
+        
+        input_width_idx = 0; output_width_idx = 0
+        while(input_width_idx < input_matrix.shape[1] - conv_filter.shape[1] + 1):
+            # Get receptive field
+            curr_region = input_matrix[input_length_idx : (input_length_idx + conv_filter.shape[0]),
+                                       input_width_idx : (input_width_idx + conv_filter.shape[1])]
+            
+            # Get sum of (dot product of receptive field and filter)
             curr_result = curr_region * conv_filter
             conv_sum = np.sum(curr_result)
-            result[r, c] = conv_sum
-            
-    #Clipping the outliers of the result matrix.
-    final_result = result[np.uint16(filter_size/2.0):result.shape[0]-np.uint16(filter_size/2.0), 
-                        np.uint16(filter_size/2.0):result.shape[1]-np.uint16(filter_size/2.0)]
-    
-    return final_result
 
-def init_filter(filter_number, filter_size, filter_channel, input_dim, input_channel):
+            output_matrix[output_length_idx, output_width_idx] = conv_sum
+
+            input_width_idx += stride
+            output_width_idx +=  1
+        #End while
+        input_length_idx += stride
+        output_length_idx += 1
+    #End while
+
+    return output_matrix
+
+def init_filter(filter_number, filter_size_length, filter_size_width, input_dim, input_channel):
     #Initialize Convulation Filter
     if (input_dim > 2):
         filter_channel = input_channel
-        conv_filter = np.zeros((filter_number, filter_size, filter_size, filter_channel))
+        conv_filter = np.zeros((filter_number, filter_size_length, filter_size_width, filter_channel))
         for i in range(filter_number):
-            conv_filter[i, :, :, :] = np.random.uniform(low=-0.1, high=0.1, size=(filter_size, filter_size, filter_channel))
+            conv_filter[i, :, :, :] = np.random.uniform(low=-0.1, high=0.1, size=(filter_size_length, filter_size_width, filter_channel))
     else:
-        conv_filter = np.zeros((filter_number, filter_size, filter_size))
+        conv_filter = np.zeros((filter_number, filter_size_length, filter_size_width))
         for i in range(filter_number):
-            conv_filter[i, :, :] = np.random.uniform(low=-0.1, high=0.1, size=(filter_size, filter_size))
+            conv_filter[i, :, :] = np.random.uniform(low=-0.1, high=0.1, size=(filter_size_length, filter_size_width))
     
     return conv_filter
     
-def conv(input_matrix, filter_number, filter_size, pad_layer, padded_number, stride):
-
+def conv(input_matrix, filter_number, filter_size_length, filter_size_width, pad_layer, padded_number, stride=1):
     #Apply padding
     input_matrix = padding(input_matrix, pad_layer, padded_number)
 
     #Initialize Convulation Filter
-    if (len(input_matrix.shape) > 2):
-        filter_channel = input_matrix.shape[-1]
-        conv_filter = np.zeros((filter_number, filter_size, filter_size, filter_channel))
-        for i in range(filter_number):
-            conv_filter[i, :, :, :] = np.random.uniform(low=-0.1, high=0.1, size=(filter_size, filter_size, filter_channel))
-    else:
-        conv_filter = np.zeros((filter_number, filter_size, filter_size))
-        for i in range(filter_number):
-            conv_filter[i, :, :] = np.random.uniform(low=-0.1, high=0.1, size=(filter_size, filter_size))
+    conv_filter = init_filter(filter_number, filter_size_length, filter_size_width, len(input_matrix.shape), input_matrix.shape[-1])
 
-    # An empty feature map to hold the output of convolving the filter(s) with the image.
-    feature_maps = np.zeros((input_matrix.shape[0]-conv_filter.shape[1]+1, 
-                                input_matrix.shape[1]-conv_filter.shape[1]+1, 
-                                conv_filter.shape[0]))
+    #Initialize bias
+    list_bias = np.zeros(conv_filter.shape[0])
 
-    if (len(input_matrix.shape) > 2):
-        filter_channel = input_matrix.shape[-1]
-        conv_filter = np.zeros((filter_number,filter_size,filter_size,filter_channel))
-        for i in range(filter_number):
-            conv_filter[i, :, :, :] = np.random.uniform(low=-0.1, high=0.1,size=(filter_size,filter_size,filter_channel))
-    else:
-        conv_filter = np.zeros((filter_number,filter_size,filter_size))
-        for i in range(filter_number):
-            conv_filter[i, :, :] = np.random.uniform(low=-0.1,high=0.1,size=(filter_size,filter_size))
+    #Feature map formula : (W - F + 2P) / S + 1
+    feature_maps_shape = (((input_matrix.shape[0] - conv_filter.shape[1] + 2 * pad_layer) // stride + 1),
+                          ((input_matrix.shape[1] - conv_filter.shape[2] + 2 * pad_layer) // stride + 1),
+                          filter_number)
+    
+    # Initialize feature map output
+    feature_maps = np.zeros(feature_maps_shape)
 
     # Convolving the image by the filter(s).
     for filter_num in range(conv_filter.shape[0]):
@@ -127,36 +96,84 @@ def conv(input_matrix, filter_number, filter_size, pad_layer, padded_number, str
         curr_filter = conv_filter[filter_num, :] # getting a filter from the bank.
 
         if len(curr_filter.shape) > 2:
-            conv_map = conv_(input_matrix[:, :, 0], curr_filter[:, :, 0], stride) # Array holding the sum of all feature maps.
+            conv_map = conv_calculation(input_matrix[:, :, 0], curr_filter[:, :, 0], stride) # Array holding the sum of all feature maps.
             for ch_num in range(1, curr_filter.shape[-1]): # Convolving each channel with the image and summing the results.
-                conv_map = conv_map + conv_(input_matrix[:, :, ch_num], 
-                                  curr_filter[:, :, ch_num], stride)
+                conv_map = conv_map + conv_calculation(input_matrix[:, :, ch_num], curr_filter[:, :, ch_num], stride)
         else: # There is just a single channel in the filter.
-            conv_map = conv_(input_matrix, curr_filter)
-        feature_maps[:, :, filter_num] = conv_map # Holding feature map with the current filter.
+            conv_map = conv_calculation(input_matrix, curr_filter)
+        
+        print(conv_map.shape)
+        feature_maps[:, :, filter_num] = conv_map + list_bias[filter_num] # Holding feature map with the current filter.
     return feature_maps # Returning all feature maps.
-    
 
-def pooling(feature_map, mode='max', size=2, stride=2):
-    #Preparing the output of the pooling operation.
-    pool_out = np.zeros((np.uint16((feature_map.shape[0]-size+1)/stride+1),
-                            np.uint16((feature_map.shape[1]-size+1)/stride+1),
-                            feature_map.shape[-1]))
-    for map_num in range(feature_map.shape[-1]):
-        r2 = 0
-        for r in np.arange(0,feature_map.shape[0]-size+1, stride):
-            c2 = 0
-            for c in np.arange(0, feature_map.shape[1]-size+1, stride):
-                if(mode=='max'):
-                    pool_out[r2, c2, map_num] = np.max([feature_map[r:r+size,  c:c+size, map_num]])
+def pool(input_matrix, pool_length, pool_width, stride, mode):
+    result_shape = (((input_matrix.shape[0] - pool_length) // stride + 1),
+                    ((input_matrix.shape[1] - pool_width) // stride + 1),
+                    input_matrix.shape[-1])
+    
+    result = np.zeros(result_shape)
+    
+    for channel in range(input_matrix.shape[-1]):
+        #input_matrix shape = (mxn)
+        region = input_matrix[:,:,channel]
+        output_shape = (((region.shape[0] - pool_length) // stride + 1),
+                        ((region.shape[1] - pool_width) // stridedef pool(input_matrix, pool_length, pool_width, stride, mode):
+    #input_matrix shape = (mxn)
+    output_shape = (((input_matrix.shape[0] - pool_length) // stride + 1),
+                    ((input_matrix.shape[1] - pool_width) // stride + 1))
+
+    output_matrix = np.zeros(output_shape)
+
+    input_length_idx = 0; output_length_idx = 0
+    while(input_length_idx < input_matrix.shape[0] - pool_length + 1):
+        
+        input_width_idx = 0; output_width_idx = 0
+        while(input_width_idx < input_matrix.shape[1] - pool_width + 1):
+            curr_region = input_matrix[input_length_idx : (input_length_idx + pool_length),
+                                       input_width_idx : (input_width_idx + pool_width)]
+            print(curr_region)
+
+            if mode == 'max':
+                curr_result = np.max(curr_region)
+            else:
+                curr_result = np.mean(curr_region)
+            
+            output_matrix[output_length_idx, output_width_idx] = curr_result
+
+            input_width_idx += stride
+            output_width_idx +=  1
+        #End while
+        input_length_idx += stride
+        output_length_idx += 1
+    #End while
+
+    return output_matrix
+        while(input_length_idx < region.shape[0] - pool_length + 1):
+
+            input_width_idx = 0; output_width_idx = 0
+            while(input_width_idx < region.shape[1] - pool_width + 1):
+                curr_region = region[input_length_idx : (input_length_idx + pool_length),
+                                           input_width_idx : (input_width_idx + pool_width)]
+
+                if mode == 'max':
+                    curr_result = np.max(curr_region)
                 else:
-                    pool_out[r2, c2, map_num] = np.mean([feature_map[r:r+size,  c:c+size, map_num]])
-                c2 = c2 + 1
-            r2 = r2 +1
-    return pool_out
+                    curr_result = np.mean(curr_region)
+
+                output_matrix[output_length_idx, output_width_idx] = curr_result
+
+                input_width_idx += stride
+                output_width_idx +=  1
+            #End while
+            input_length_idx += stride
+            output_length_idx += 1
+        #End while
+        
+        result[:,:,channel] = output_matrix
+
+    return result
 
 def relu(feature_map):
-    #Preparing the output of the ReLU activation function.
     relu_out = np.zeros(feature_map.shape)
     for map_num in range(feature_map.shape[-1]):
         for r in np.arange(0,feature_map.shape[0]):
@@ -170,7 +187,7 @@ def flatten(output_layer):
 
 def dense(dense_input, class_num, activation_func="sigmoid"):
     def sigmoid_dense(net):
-        return np.float64(1 / (1 + np.exp(-net)))
+        return 1. / (1. + np.exp(-net))
 
     def relu_dense(x):
         if(x < 0):
